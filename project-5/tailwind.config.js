bf5fcf2e6c4a41aadfe5f109aeaac04e5861051b
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-03-05 09:19:28 +0530

    working with tailwind creating new projects.

---
+++ project-5/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./dist/index.htm"],
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
